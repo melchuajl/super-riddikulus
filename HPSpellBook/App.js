6941d803ef186145a7a40ce5aeb1f6843de897d6
Melissa Chua <102275204+melchuajl@users.noreply.github.com>
2022-08-15 09:19:10 +0800

    Checking pull request function (#1)
    * testing out wip branch
    
    * added component diagram and new components

--- HPSpellBook/App.js
+++ HPSpellBook/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
+import styles from './src/styles/Stylesheet';
 import SpellTypes from './src/components/SpellTypes';
 
 export default function App() {
@@ -11,12 +12,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
