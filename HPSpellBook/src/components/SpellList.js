c9394e56bf1cc3e184ec626654bdbdf3cf79711a
Farhan <79401671+farhansamsuri@users.noreply.github.com>
2022-08-15 22:58:31 +0800

    Farhan (#2)
    * feature updates
    -add list according to spellType
    -add key to each item on list
    
    * minor update

---
+++ HPSpellBook/src/components/SpellList.js
@@ -0,0 +1,38 @@
+import API from "../../API";
+import { useEffect, useState } from "react";
+import { View, Text, ScrollView } from 'react-native';
+import uuid from 'react-native-uuid';
+
+const SpellList = () => {
+
+    const [spellList, setSpellList] = useState([]);
+
+    const getSpellList = async () => {
+        const { status, data } = await API.get('/Spells');
+        const spellList = data;
+
+        if (status === 200) {
+            setSpellList(spellList);     
+        }
+    }
+
+    useEffect(() => {
+        getSpellList();
+    }, []);
+
+    return (
+        <View>
+            <ScrollView >
+                {spellList.filter(spell => spell.type === 'Curse').map(filteredSpell => (
+                        <ScrollView key = {uuid.v4()}>
+                        <Text>{filteredSpell.name}</Text>
+                        </ScrollView>
+                ))  
+                }
+            </ScrollView>
+        </View>
+    );
+
+}
+
+export default SpellList; 
