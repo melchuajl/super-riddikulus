a02167f3546fe34acf6a815ae05be7544131e7e1
melchuajl <102275204+melchuajl@users.noreply.github.com>
2022-08-19 02:27:16 +0800

    minor changes

--- HPSpellBook/App.js
+++ HPSpellBook/App.js
@@ -5,13 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainScreen from './src/screens/MainScreen';
 import TTS from './src/components/TTS';
 import SpellTypeScreen from './src/screens/SpellTypeScreen';
+import SpellList from './src/components/SpellList';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Welcome'>
+            <Stack.Navigator initialRouteName='SpellList'>
                 <Stack.Screen name='TTS' component={TTS} screenOptions={{ headerShown: false }} />
                 <Stack.Screen name='Welcome' component={MainScreen} screenOptions={{ headerShown: false }} />
                 <Stack.Screen name='SpellTypes' component={SpellTypeScreen} options={{ headerShown: false }} />
