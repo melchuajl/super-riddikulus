7d46b4906c9a7350066cc41033a4985d2946c3bf
Farhan <mdfarhansamsuri@gmail.com>
2022-08-19 13:05:05 +0800

    add audio and fade animation to welcomeScreen

--- HPSpellBook/src/screens/WelcomeScreen.js
+++ HPSpellBook/src/screens/WelcomeScreen.js
@@ -1,15 +1,67 @@
-import { View, TouchableOpacity, ImageBackground, Text } from 'react-native';
+import React, {useRef, useEffect, useState} from "react";
+import { View, TouchableOpacity, ImageBackground, Text, Animated } from 'react-native';
 import styles from '../styles/FarhanStyle';
 import welcome from '../../assets/welcomeImage.png'
 import { useNavigation } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import { Audio } from 'expo-av';
 
 const WelcomeScreen = () => {
 
+    const [sound, setSound] = useState();
+
+    async function playSound() {
+    const {sound} = await Audio.Sound.createAsync(
+        require('../../assets/audio/HP-theme-Marimba.mp3'),
+        { shouldPlay: true}
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+    }
+
+    const fadeAnim = useRef(new Animated.Value(0)).current;
     const navigation = useNavigation();
     const [fontsLoaded] = useFonts({
         'CroissantOne': require('../../assets/fonts/CroissantOne.ttf'),
     });
+
+    useEffect(() => {
+  
+        playSound();
+
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(
+                    fadeAnim,
+                    {
+                        toValue:1,
+                        duration:0,
+                        useNativeDriver: true
+                    }),
+                    Animated.timing(
+                        fadeAnim, {
+                            toValue: 0,
+                            delay: 0,
+                            duration: 1200,
+                            useNativeDriver: true
+                        }),
+                        Animated.timing(
+                            fadeAnim,
+                            {
+                                toValue:1,
+                                duration:1200,
+                                useNativeDriver: true
+                            }),
+                    
+            ]),
+            {
+                iterations: 20
+            }
+            
+            ).start()
+    }, [fadeAnim])
+
     if (!fontsLoaded) {
         return null;
     }
@@ -21,7 +73,9 @@ const WelcomeScreen = () => {
                 <TouchableOpacity
                     onPress={() => { navigation.navigate('SpellTypes') }}
                     hitSlop={{ top: 1000, bottom: 1000, left: 1000, right: 1000 }}>
-                    <Text style={styles.welcomeText}>Tap Anywhere To Begin</Text>
+                    <Animated.View style={{opacity: fadeAnim}}>
+                        <Text style={styles.welcomeText}>Tap Anywhere To Begin</Text>
+                        </Animated.View>
                 </TouchableOpacity>
             </ImageBackground>
         </View>
