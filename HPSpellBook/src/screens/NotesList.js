afb5ed531d3d94d7d438695e7e018ac2f1f39ac3
niansoon <simniansoon@hotmail.com>
2022-09-13 16:16:43 +0800

    test

--- HPSpellBook/src/screens/NotesList.js
+++ HPSpellBook/src/screens/NotesList.js
@@ -6,9 +6,12 @@ import { useNavigation } from '@react-navigation/native';
 import { useRoute } from "@react-navigation/native";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import uuid from 'react-native-uuid';
+import noteslistBg from '../../assets/notes/noteslistBg.png';
+import userProfileBar from '../../assets/notes/userProfileBar.png';
+import blackbar from '../../assets/notes/blackbar.png'
 
 import styles from "../styles/Stylesheet";
-import backgroundImg from '../../assets/bgImage1.png';
+import notesBG from '../../assets/notes/notesBG.png';
 import categorybar from '../../assets/categorybar.png';
 import NoteCard from '../components/NoteCard';
 import TabNav from '../components/TabNav';
@@ -51,44 +54,60 @@ const NotesList = () => {
 
     return (
         <View>
+
             <ImageBackground
-                source={backgroundImg}
+                source={notesBG}
                 style={styles.bg}>
+
                 <ImageBackground
-                    source={categorybar}
-                    style={styles.bar}>
-                    <View style={styles.divider}></View>
-                    <View style={styles.divider2}></View>
-                    <Text style={styles.header}>Notes List</Text>
-                    <View style={styles.divider3}></View>
-                    <View style={styles.divider4}></View>
-                </ImageBackground>
-                <View style={styles.noteContainer}>
-
-                    <TouchableOpacity
-                        onPress={() => { navigation.navigate('NotesInput') }}>
-                        <Text style={[styles.search, { marginLeft: -30 }]}>
-                            <Icon name='add-circle' size={15} /> Add note
-                        </Text>
-                    </TouchableOpacity>
-
+                    source={userProfileBar}
+                    style={styles.userBar}
+                >
                     <TouchableOpacity
                         onPress={() => { logout() }}>
-                        <Text style={[styles.search, { marginLeft: -30 }]}>
-                            <Icon name='add-circle' size={15} /> Sign Out
+                        <Text style={styles.logout}>
+                            Logout
                         </Text>
                     </TouchableOpacity>
+                </ImageBackground>
 
+                    <ImageBackground
+                    style={styles.blackbar}
+                        source={blackbar}>
+                            <Text style={styles.blackbarText}>TESTING 123</Text>
+                    </ImageBackground>
+
+                <ImageBackground>
+                    <Text style={styles.notesheader}>Notes List</Text>
+                </ImageBackground>
+
+                <View style={styles.noteContainer}>
                     <ScrollView>
-                        {notesList.map(n =>
-                            <NoteCard
-                                key={uuid.v4()}
-                                title={n.title}
-                                date={n.updatedAt}
-                                preview={n.body}
-                                id={n._id}
-                            />)
-                        }
+                        <ImageBackground
+                            source={noteslistBg}
+                            style={styles.noteslist}
+                        >
+
+                            <TouchableOpacity
+                                onPress={() => { navigation.navigate('NotesInput') }}>
+                                <Text style={[styles.search, { marginLeft: 110 }]}>
+                                    <Icon name='add-circle' size={15} /> Add note
+                                </Text>
+                            </TouchableOpacity>
+
+                            {notesList[0] ?
+                                notesList.map(n =>
+                                    <NoteCard
+                                        key={uuid.v4()}
+                                        title={n.title}
+                                        date={n.updatedAt}
+                                        preview={n.body}
+                                        id={n._id}
+                                    />)
+                                :
+                                <Text>Add your first note</Text>
+                            }
+                        </ImageBackground>
                     </ScrollView>
                 </View>
 
