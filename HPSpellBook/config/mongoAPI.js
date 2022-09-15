3ac6fdb7f747a8d5613b29ab082368b730d54fab
melchuajl <102275204+melchuajl@users.noreply.github.com>
2022-09-15 10:25:10 +0800

    pre-presentation commit

--- HPSpellBook/config/mongoAPI.js
+++ HPSpellBook/config/mongoAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const mongoAPI = axios.create({baseURL:"https://super-riddikulus-server.herokuapp.com/"});
+// const mongoAPI = axios.create({baseURL:"https://super-riddikulus-server.herokuapp.com/"});
+
+export const mongoAPI = axios.create({baseURL:"http://localhost:3008"});
 
 export default mongoAPI;
